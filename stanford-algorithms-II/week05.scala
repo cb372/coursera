0d4262c13d336f0ca73efa226db7102601587ec9
Chris Birchall <chris.birchall@gmail.com>
2013-10-10 22:21:20 +0900

    Finish week 5

--- stanford-algorithms-II/week05.scala
+++ stanford-algorithms-II/week05.scala
@@ -12,6 +12,7 @@ val nodes: Array[Node] = {
 }
 val n = nodes.size
  
+/** Euclidean distance between the points at the given indices */
 def cost(i1: Int, i2: Int): Float = {
   import Math._
   val from = nodes(i1)
@@ -19,6 +20,9 @@ def cost(i1: Int, i2: Int): Float = {
   sqrt(pow(from._1 - to._1, 2) + pow(from._2 - to._2, 2)).toFloat
 }
 
+/** Recursively calculate and memoize all values of aCb
+ *  for 1 <= a, b <= n
+ */
 def memoizeBinomCoeffs(max: Int) = {
   val result = collection.mutable.Map.empty[(Int, Int), Int]
   // base cases
@@ -35,10 +39,16 @@ def memoizeBinomCoeffs(max: Int) = {
   result.toMap
 }
 val binomCoeffs: Map[(Int, Int), Int] = memoizeBinomCoeffs(n)
+
+/** n C k */
 def choose(n: Int, k: Int): Int = binomCoeffs.getOrElse((n, k), 0)
 
+/** Check if the given bit is set in the given bitset */
 def bitIsSet(bitset: Int, k: Int): Boolean = (bitset & (1 << k)) != 0
 
+/** Decide the array index of the given bitset using combinatorial numbering:
+ *  N = choose(c_k, k) + ... + choose(c_1, 1)
+ */
 def index(combination: Int): Int = {
   var result = 0
   var k = 1
@@ -51,6 +61,7 @@ def index(combination: Int): Int = {
   result
 }
 
+/** Loop through all choose(n, k) bitsets */
 def loopCombinations(n: Int, k: Int)(f: Int => Unit): Unit = {
   var set = (1 << k) - 1
   val limit = (1 << n)
@@ -65,6 +76,7 @@ def loopCombinations(n: Int, k: Int)(f: Int => Unit): Unit = {
   }
 }
 
+/** Turn a bitset into an array of node IDs for debugging */
 def toNodeArray(S: Int): Array[Int] = {
   var result: List[Int] = Nil
   for (i <- 0 until n) 
@@ -73,40 +85,41 @@ def toNodeArray(S: Int): Array[Int] = {
   result.toArray
 }
 
-var prev = Array.ofDim[Float](choose(n, 1), n)
-prev(index(1))(0) = 0F
-for(i <- 1 until n) {
-  val S = 1 << i
-  prev(index(S))(0) = HUGE
-}
-println(s"m = 1, choose(n, m) = ${choose(n, 1)}")
-for (i <- 0 until n) {
-  val S = 1 << i
-  println(s"A[${toNodeArray(S).mkString("(", ",", ")")}] = ${prev(index(S)).mkString("\t")}")
-}
+/* 2-D array built during previous iteration of the m-loop */
+var prev: Array[Array[Float]] = null
 
+/* 2-D array that we are currently building */
 var curr: Array[Array[Float]] = null
+
+/** Lookup a value in the 2-D array, 
+ *  with special treatment for the base cases
+ */
+def lookup(i: Int, k: Int, m: Int): Float = {
+  //println(s"lookup($i, $k, $m)")
+  if (m == 2) {
+    if (i== 0 && k == 0) return 0F // base case 1
+    else return HUGE
+  } 
+  else if (k == 0) return HUGE // base case 2
+  else return prev(i)(k - 1)
+}
 
 for (m <- 2 to n) {
   println(s"m = $m, choose(n, m) = ${choose(n, m)}")
-  curr = Array.ofDim[Float](choose(n, m), n)
+  curr = Array.fill[Float](choose(n, m), n - 1)(HUGE)
   loopCombinations(n, m){ S =>
     if (bitIsSet(S, 0)) {
-      val nodes = toNodeArray(S)
-      for (i <- 0 until nodes.size) {
-        val j = nodes(i)
-        if (j != 0) {
-          val S_minus_j = S & ~(1 << j)
-          var min = HUGE
-          for (l <- (0 until nodes.size) if l != i) {
-            val k = nodes(l)
-            val value = prev(index(S_minus_j))(k) + cost(k, j)
-            min = min.min(value)
-          }
-          curr(index(S))(j) = min
+      for(j <- (1 until n) if bitIsSet(S, j)) { // for j in S, j != 1
+        val S_minus_j = S & ~(1 << j)
+        var min = HUGE
+        // A[S, j] = min over k in S, k !=j { A[S-{j}, k] + C_kj }
+        for (k <- (0 until n) if k != j && bitIsSet(S, k)) { 
+          val value = lookup(index(S_minus_j), k, m) + cost(k, j)
+          min = min.min(value)
         }
+        curr(index(S))(j - 1) = min
       }
-      println(s"A[${toNodeArray(S).mkString("(", ",", ")")}] = ${curr(index(S)).mkString("\t")}")
+      //println(s"A[${toNodeArray(S).mkString("(", ",", ")")} = ${index(S)}] = ${curr(index(S)).mkString("\t")}")
     }
   }
   prev = curr
@@ -114,7 +127,7 @@ for (m <- 2 to n) {
 
 var result = HUGE
 for (j <- 1 until n) {
-  println(s"${curr(0)(j)} + ${cost(j, 1)} = curr(0)(j) + cost(j, 1)")
-  result = result.min(curr(0)(j) + cost(j, 1))
+  //println(s"curr(0)(j-1) + cost(j, 0) = ${curr(0)(j-1)} + ${cost(j, 0)}")
+  result = result.min(curr(0)(j-1) + cost(j, 0))
 }
-println(result)
+println(result.toInt)
